af45b0217d835df3deadf2f8f78d623343419ebf
Faahim Sadman <57841708+SadManFahIm@users.noreply.github.com>
2021-06-26 03:03:55 +0600

    Own Commit

---
+++ Arrays Left Rotation.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#define MAX 100000
+
+ int
+main (void) 
+{
+  
+int a[MAX], i, n, d;
+  
+ 
+scanf ("%d%d", &n, &d);
+  
+ 
+for (i = 0; i < n; i++)
+    
+scanf ("%d", &a[i]);
+  
+ 
+for (i = 0; i < n; i++)
+    
+printf ("%d ", a[(i + d) % n]);
+  
+    
+return 0;
+
+}
