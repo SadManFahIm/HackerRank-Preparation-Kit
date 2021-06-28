2c160911b4d67e3e88be6d509008977c12bbed09
Faahim Sadman <57841708+SadManFahIm@users.noreply.github.com>
2021-06-28 23:25:47 +0600

    Own Commit

---
+++ Has Tables.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+  int m, n;
+  cin >> m >> n;
+
+  map<string, int> magazine;
+
+  for (int i = 0; i < m; i++) {
+    string word;
+    cin >> word;
+
+    if (magazine.find(word) == magazine.end()) {
+      magazine[word] = 1;
+    } else {
+      magazine[word]++;
+    }
+  }
+
+  bool ans = true;
+  for (int i = 0; i < n; i++) {
+    string word;
+    cin >> word;
+
+    if (magazine.find(word) == magazine.end() || magazine[word] == 0) {
+      ans = false;
+      break;
+    } else {
+      magazine[word]--;
+    }
+  }
+
+  if (ans)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  return 0;
+}
